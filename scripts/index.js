d02a54cba07affe589dbaa95df37f422b63a4143
Andrey Zaderenko <andrey42work@gmail.com>
2023-08-15 23:32:04 +0300

    fix: new review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,30 @@
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.profileForm'); // Воспользуйтесь методом querySelector()
 let nameInput = document.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.popup__input_type_description')// Воспользуйтесь инструментом .querySelector()
 let profileEditBtn = document.querySelector('.profile__edit-btn');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
 // открыть попап
 profileEditBtn.addEventListener('click', () => {
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
-    popup.classList.add('popup_opened');
+    openPopup();
 });
 
 // закрыть без сохранения
 popupCloseBtn.addEventListener('click', () => {
-    popup.classList.remove('popup_opened');
+    closePopup();
 });
 
 // Обработчик «отправки» формы, хотя пока
@@ -25,7 +33,7 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;  
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
