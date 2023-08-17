3c6670ad2589c9d6ccb14ff77af86f4c61b68c3d
Andrey Zaderenko <andrey42work@gmail.com>
2023-08-17 13:30:00 +0300

    feat: initial cards generation

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,55 @@ let jobInput = document.querySelector('.popup__input_type_description')// Вос
 let profileEditBtn = document.querySelector('.profile__edit-btn');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 
+const initialCards = [
+    {
+      name: 'Фэшен',
+      link: 'https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=282'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Бэтмен',
+      link: 'https://images.unsplash.com/photo-1531259683007-016a7b628fc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=282&q=282'
+    }
+  ]; 
+const cardsSection = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#cardTemplate');
+
+renderCards(initialCards);
+
+// создаем 6 карточек при запуске страницы
+function renderCards(initialData) {
+  initialData.forEach((item) => {
+    cardsSection.append(createCardTemplate(item));
+  });
+};
+
+// настраиваем темплейт
+function createCardTemplate(cardData) {
+  const el = cardTemplate.content.cloneNode(true);
+  let cardHeader = el.querySelector('.card__header');
+  let cardImage = el.querySelector('.card__img');
+  cardHeader.textContent = cardData.name;
+  cardImage.setAttribute('src', cardData.link);
+
+  return el;
+}
+
 function openPopup() {
     popup.classList.add('popup_opened');
 }
